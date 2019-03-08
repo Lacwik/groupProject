018c7e1b1d58f268d9bd748e65df7289d09e5681
komura92 <komura92@gmail.com>
2019-03-08 17:59:11 +0100

    a

--- webApp/calculator/src/main/java/com/b/calculator/controller/CalculatorController.java
+++ webApp/calculator/src/main/java/com/b/calculator/controller/CalculatorController.java
@@ -1,6 +1,6 @@
 package com.b.calculator.controller;
 
-import com.b.calculator.repository.VegetableRepository;
+import com.b.calculator.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +11,24 @@ public class CalculatorController {
     @Autowired
     VegetableRepository vegetableRepository;
 
+    @Autowired
+    LineRepository lineRepository;
+
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    ResourceRepository resourceRepository;
+
+    @Autowired
+    StageRepository stageRepository;
+
 
     @GetMapping("/home")
     public String home()
     {
          return "home";
     }
+
+
 }
