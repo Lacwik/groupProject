e42ac450c7b85fd2206b8ccd0d666fc962d0a245
komura92 <komura92@gmail.com>
2019-03-08 15:40:45 +0100

    Trying..

---
+++ webApp/calculator/src/main/java/com/b/calculator/config/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.b.calculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+
+        return bean;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index");
+    }
+
+}
+
--- webApp/calculator/src/main/java/com/b/calculator/controller/CalculatorController.java
+++ webApp/calculator/src/main/java/com/b/calculator/controller/CalculatorController.java
@@ -2,9 +2,17 @@ package com.b.calculator.controller;
 
 import com.b.calculator.repository.VegetableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class CalculatorController {
 
     @Autowired
     VegetableRepository vegetableRepository;
+
+
+    @GetMapping("/home")
+    public String home()
+    {
+         return "home";
+    }
 }
