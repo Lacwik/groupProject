7de78d3662b70dda28f58d0ce098ee11034f034a
Klimek <kamilklimek97@gmail.com>
2019-04-02 19:42:32 +0100

    Fix line facade

--- webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/line/LineFacade.java
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/line/LineFacade.java
@@ -1,11 +1,9 @@
 package com.wfiis.CalculatorCO2.line;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class LineFacade {
 }
