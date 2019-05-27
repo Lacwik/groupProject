56078004fe17432d95097bf109a0bc70fa8dbe99
wygwizdany <faketofake@wp.pl>
2019-05-27 15:01:47 +0200

    tomek ty kurwo

--- webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/resource/model/ResourceModel.java
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/resource/model/ResourceModel.java
@@ -2,6 +2,8 @@ package com.wfiis.CalculatorCO2.resource.model;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class ResourceModel extends ResourceCreateModel{
     private Long id;
@@ -9,5 +11,18 @@ public class ResourceModel extends ResourceCreateModel{
     public ResourceModel(String name, String gus, long id){
         super(name, gus);
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResourceModel that = (ResourceModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/vegetable/model/VegetableModel.java
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/vegetable/model/VegetableModel.java
@@ -2,6 +2,8 @@ package com.wfiis.CalculatorCO2.vegetable.model;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class VegetableModel extends VegetableCreateModel{
     private Long id;
@@ -9,5 +11,18 @@ public class VegetableModel extends VegetableCreateModel{
     public VegetableModel(String name, long id){
         super(name);
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VegetableModel that = (VegetableModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
