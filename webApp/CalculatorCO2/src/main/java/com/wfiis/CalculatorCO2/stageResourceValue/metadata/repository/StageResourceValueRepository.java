72b3b7e0a86d3d831325a321ca705f5d107e6d30
komura92 <komura92@gmail.com>
2019-06-03 12:17:45 +0200

    Stage Resource Value

---
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/stageResourceValue/metadata/repository/StageResourceValueRepository.java
@@ -0,0 +1,7 @@
+package com.wfiis.CalculatorCO2.stageResourceValue.metadata.repository;
+
+import com.wfiis.CalculatorCO2.stageResourceValue.metadata.entity.StageResourceValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StageResourceValueRepository extends JpaRepository<StageResourceValue, Long> {
+}
---
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/stageResourceValue/model/StageResourceValueCreateModel.java
@@ -0,0 +1,17 @@
+package com.wfiis.CalculatorCO2.stageResourceValue.model;
+
+import com.wfiis.CalculatorCO2.resource.metadata.entity.Resource;
+import com.wfiis.CalculatorCO2.stage.metadata.entity.Stage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class StageResourceValueCreateModel {
+    private Resource resource;
+    private float value;
+    private Stage stage;
+    private float time;
+}
