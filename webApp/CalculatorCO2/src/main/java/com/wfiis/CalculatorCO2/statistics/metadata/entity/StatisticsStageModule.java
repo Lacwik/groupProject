ea77402a7953d7e598a847d6257aa1ea1530f69e
Kamil Klimek <klimekk@tt.com.pl>
2019-06-09 08:12:42 +0200

    Add charts to statistic

--- webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/statistics/metadata/entity/StatisticsStageModule.java
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/statistics/metadata/entity/StatisticsStageModule.java
@@ -1,5 +1,6 @@
 package com.wfiis.CalculatorCO2.statistics.metadata.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.wfiis.CalculatorCO2.stage.metadata.entity.Stage;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,16 +9,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -25,25 +23,18 @@ import java.util.List;
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-@Table(name = "calc_line_statistics_stage")
-public class StatisticsStage {
+@Table(name = "calc_line_statistics_stage_module")
+public class StatisticsStageModule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @ManyToOne
-    @JoinColumn(name = "stage_id")
-    private Stage stage;
+    @JoinColumn(name = "statistic_stage_id")
+    @JsonBackReference
+    private StatisticsStage statisticsStage;
 
-    @OneToMany(cascade= CascadeType.ALL)
-    private List<StatisticsStageResource> stageResources;
-
-    @OneToMany(cascade=CascadeType.ALL)
-    private List<StatisticsStageLeftover> stageLeftovers;
-
-    @OneToMany(cascade=CascadeType.ALL)
-    private List<StatisticsStageModule> statisticsStageModules;
-
-    private Long time;
-    private Float carbonPrint;
+    private String name;
+    private float power;
 }
---
+++ webApp/CalculatorCO2/src/main/java/com/wfiis/CalculatorCO2/statistics/metadata/repository/StatisticsStageModuleRepository.java
@@ -0,0 +1,7 @@
+package com.wfiis.CalculatorCO2.statistics.metadata.repository;
+
+import com.wfiis.CalculatorCO2.statistics.metadata.entity.StatisticsStageModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsStageModuleRepository extends JpaRepository<StatisticsStageModule, Long> {
+}
